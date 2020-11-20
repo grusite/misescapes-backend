b47f8c48af5d50367cc17c6c772ad08ba5eb74ba
Jorge Martín <grusite@gmail.com>
2020-11-21 00:54:54 +0100

    protected routes, added jwt guards and strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,7 +7,7 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User, UserSchema } from './schemas/user.schema';
-import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtStrategy } from './strategies/jwt-auth.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
